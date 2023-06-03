ce07ae1585a9548c49c0ff90a12d46451c784c47
nreece6 <reece.nick24@gmail.com>
2023-06-03 16:13:56 -0500

    worked on cateogry-routes get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,32 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+  const categories = await Category.findAll({include: [{ model: Product}] })
+  res.status(200).json(categories)
+  } catch (err) {
+    res.status(500).json({message: 'Server Error'})
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, { include: [{ model: Product}] })
+  
+  if(!category) {
+    res.status(404).json({message: 'ID Not Found'})
+    return
+  }
+  res.status(200).json(category)
+  } 
+  catch (err) {
+    res.status(500).json({message: 'Server Error'})
+  }
 });
 
 router.post('/', (req, res) => {
