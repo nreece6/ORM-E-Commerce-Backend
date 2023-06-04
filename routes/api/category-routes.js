d2dee5605c9967342b88286ae242dc792eefdf69
nreece6 <reece.nick24@gmail.com>
2023-06-04 16:06:29 -0500

    updated category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,16 +31,37 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body)
+    res.status(200).json(newCategory)
+  }
+  catch (err) {
+    res.status(500).json({message: 'Server Error'})
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updatedCategory = await Category.update(req.body, { where: {id: req.params.id} })
+    !updatedCategory ? res.status(404).json({ message: 'ID Not Found' }) : res.status(200).json(updatedCategory)
+  }
+  catch (err) {
+    res.status(500).json({message: 'Server Error'})
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy(req.params.id)
+    !deletedCategory ? res.status(404).json({ message: 'ID Not Found' }) : res.status(200).json(deletedCategory)
+  }
+  catch (err) {
+    res.status(500).json({message: 'Server Error'})
+  }
 });
 
 module.exports = router;
