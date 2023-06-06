cd6856bd77ac608e8955aa0d3a85a7232c579e56
nreece6 <reece.nick24@gmail.com>
2023-06-06 16:29:20 -0500

    more tag routes updates

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,12 +39,26 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(req.body, {where: { id: req.params.id } })
+    !updatedTag ? res.status(404).json({ message: 'Tag Not Found'}) : res.status(200).json(updatedTag)
+  }
+  catch (err) {
+    res.status(500).json({message: 'Server Error'})
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy(req.params.id)
+    !deletedTag ? res.status(404).json({ message: 'Tag Not Found' }) : res.status(200).json(deletedTag)
+    }
+    catch (err) {
+      res.status(500).json({message: 'Server Error'})
+    }
 });
 
 module.exports = router;
